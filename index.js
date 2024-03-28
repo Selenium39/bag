51e54882e23f41fc17bea51cbf5eb0c09e88bb32
selenium39 <selenium39@qq.com>
2024-03-28 17:07:29 +0800

    能够查看小说章节和从指定章节开始阅读

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { Command } = require('commander');
-const { addNovel,listNovel,readNovel } = require('./lib/novel');
+const { addNovel,listNovel,readNovel,listChapters } = require('./lib/novel');
 const pkg = require('./package.json') 
 const program = new Command();
 
@@ -19,9 +19,14 @@ program
   .description('列出所有小说')
   .action(listNovel);
 
-  program
-  .command('read <novelName>')
+program
+  .command('read <novelName> [chapterIndex]')
   .description('阅读指定的小说')
   .action(readNovel);
+
+program
+  .command('chapters <novelName>')
+  .description('列出指定小说的所有章节')
+  .action(listChapters);
 
 program.parse(process.argv);
